09b44692a8a3a6ea28ceed67073af39c000782a8
bill lam <bbill.lam@gmail.com>
2013-03-02 15:03:59 +0800

    wd qwd

--- lib/wd/wd.cpp
+++ lib/wd/wd.cpp
@@ -450,6 +450,9 @@ void wdqueries(string s)
     int dia=sqrt(dpix*dpix+dpiy*dpiy);
     result=i2s(mmx) + " " + i2s(mmy) + " " + i2s(w) + " " + i2s(h) + " " + i2s(dpix) + " " + i2s(dpiy) + " " + i2s(QApplication::desktop()->depth()) + " 1 " + i2s(QApplication::desktop()->colorCount()) + " " + i2s(dpix) + " " + i2s(dpix) + " " + i2s(dia);
     return;
+  } else if (s=="qwd") {
+    result="jqt";
+    return;
   } else if (s=="qpx") {
     if (!Forms.size()) result="";
     else {
