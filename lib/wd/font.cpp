9aada892752b688c0f9c7a5308b6d8945ffc1bb2
bill lam <bbill.lam@gmail.com>
2013-06-02 08:23:06 +0800

    glfont: empty string

--- lib/wd/font.cpp
+++ lib/wd/font.cpp
@@ -11,27 +11,29 @@ Font::Font(string s)
   angle=0;
   QString face, sizestyle;
   int bold=0,italic=0,strikeout=0,underline=0;
-  float size=1;
+  float size=0;
   QString textstring = QString::fromUtf8 (s.c_str());
   if (textstring.startsWith("\"")) {
     QStringList ss=textstring.split("\"",QString::SkipEmptyParts);
-    face = ss[0];
-    if (ss.length()>1) {
-      sizestyle = ss[1];
-      QStringList ss1=sizestyle.split(" ",QString::SkipEmptyParts);
-      for (int j = 0; j < ss1.length(); j++) {
-        if (ss1[j]=="bold") bold = 1;
-        else if (ss1[j]=="italic") italic = 1;
-        else if (ss1[j]=="underline") underline = 1;
-        else if (ss1[j]=="strikeout") strikeout = 1;
-        else if (ss1[j].mid(0,5)=="angle") angle = c_strtoi(q2s(ss1[j].mid(5)));
-        else size = ss1[j].toFloat();
+    if (ss.length()>0) {
+      face = ss[0];
+      if (ss.length()>1) {
+        sizestyle = ss[1];
+        QStringList ss1=sizestyle.split(" ",QString::SkipEmptyParts);
+        for (int j = 0; j < ss1.length(); j++) {
+          if (ss1[j]=="bold") bold = 1;
+          else if (ss1[j]=="italic") italic = 1;
+          else if (ss1[j]=="underline") underline = 1;
+          else if (ss1[j]=="strikeout") strikeout = 1;
+          else if (ss1[j].mid(0,5)=="angle") angle = c_strtoi(q2s(ss1[j].mid(5)));
+          else size = (float) c_strtod(q2s(ss1[j]));
+        }
       }
     }
   } else {
     QStringList ss=textstring.split(" ",QString::SkipEmptyParts);
-    face = ss[0];
-    if (ss.length()>1) {
+    if (ss.length()>0) {
+      face = ss[0];
       if (ss.length()>1) {
         for (int j = 1; j < ss.length(); j++) {
           if (ss[j]=="bold") bold = 1;
@@ -39,7 +41,7 @@ Font::Font(string s)
           else if (ss[j]=="underline") underline = 1;
           else if (ss[j]=="strikeout") strikeout = 1;
           else if (ss[j].mid(0,5)=="angle") angle = c_strtoi(q2s(ss[j].mid(5)));
-          else size = ss[j].toFloat();
+          else size = (float) c_strtod(q2s(ss[j]));
         }
       }
     }
@@ -47,7 +49,7 @@ Font::Font(string s)
 
 //  qDebug() << "font: " + face + ",size=" + QString::number(size) + ",bold=" + QString::number(bold) + ",italic=" + QString::number(italic) + ",strikeout=" + QString::number(strikeout) + ",underline=" + QString::number(underline) + ",angle=" + QString::number(angle) ;
   font = QFont (face);
-  font.setPointSizeF(size);
+  if (0.0!=size) font.setPointSizeF((size>0.0)?size:-size);
   font.setBold(bold);
   font.setItalic(italic);
   font.setStrikeOut(strikeout);
