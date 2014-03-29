86288640f9f82042a1ffe797cd686a6424547b43
bill lam <bbill.lam@gmail.com>
2014-03-29 23:38:47 +0800

    android use browse_j_ in menuhelp

--- lib/base/menuhelp.cpp
+++ lib/base/menuhelp.cpp
@@ -273,8 +273,8 @@ void htmlhelp(QString s)
   QString t=cpath("~addons/docs/help/") + s + ".htm";
 #ifdef QT_OS_ANDROID
   if (QFile(t).exists())
-    android_exec_host((char *)"android.intent.action.VIEW",t.prepend("file://").toUtf8().data(),(char *)"text/html",0x00040000);
-  else android_exec_host((char *)"android.intent.action.VIEW",s.prepend("http://www.jsoftware.com/help/").append(".htm").toUtf8().data(),(char *)"text/html",0x00040000);
+    tedit->docmds("browse_j_ '" + t.prepend("file://") + "'", false);
+  else tedit->docmds("browse_j_ '" + s.prepend("http://www.jsoftware.com/help/").append(".htm") + "'", false);
 #else
   if (QFile(t).exists())
     QDesktopServices::openUrl(QUrl::fromLocalFile(t));
