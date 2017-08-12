aa38d8516e5cc583f600c72439dccb293b1d4724
bill lam <bbill.lam@gmail.com>
2017-08-12 22:17:13 +0800

    makeall32.sh comment for cross compilation

--- makeall32.sh
+++ makeall32.sh
@@ -2,14 +2,18 @@
 #
 # run in jqt directory
 
+# download and install Qt dev for linux x86
+# and amend the next line
+QM=$HOME/Qt/5.3-linux32/5.3/gcc/bin/qmake
+
 S=$(dirname "$0")
 
 ./clean.sh
 ./clean.l32
 
 cd lib
-qmake -spec linux-cross && make
+qmake -spec linux-g++-32 && make
 cd ..
 
 cd main
-qmake -spec linux-cross && make
+qmake -spec linux-g++-32 && make
