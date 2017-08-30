edb95f5f5798c0d8f630f46a3989e0eebb1a3f4e
bill lam <bbill.lam@gmail.com>
2017-08-30 09:09:20 +0800

    remove unused shell scripts

--- makeallall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export QT_SELECT=4
-
-./makeall.sh
-./makeall32.sh
-./makewin64.sh
-./makewin.sh
-./makeraspi.sh
--- makeallwine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export QT_SELECT=4
-
-./makeall.sh
-./makeall32.sh
-./makewine64.sh
-./makewine32.sh
-./makeraspi.sh
