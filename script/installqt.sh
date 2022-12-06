87e86f80e850909bbe7d163d8280bfa46976845e
bill lam <bbill.lam@gmail.com>
2022-12-06 13:21:47 +0800

    test

--- script/installqt.sh
+++ script/installqt.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-f() { sudo apt-get install -y "$@"; }
+f() { sudo apt-get install --no-install-recommends -y "$@"; }
 
-if [ "$1" != "raspberry32xx" ]; then
+if [ "$1" != "raspberry32" ]; then
 f libpulse-dev
 f qtbase5-dev qtbase5-dev-tools
 f qtmultimedia5-dev libqt5multimediawidgets5
@@ -10,10 +10,6 @@ f libqt5opengl5 libqt5opengl5-dev
 f libqt5svg5 libqt5svg5-dev
 f qtwebengine5-dev libqt5websockets5-dev
 else
-f libpulse-dev
-f qtbase4-dev qtbase4-dev-tools
-f qtmultimedia4-dev libqt4multimediawidgets4
-f libqt4opengl4 libqt4opengl4-dev
-f libqt4svg4 libqt4svg4-dev
-f qtwebkit4-dev libqt4webkit4-dev
+f libqt4-dev libqt4-svg qt4-make
+f libqtwebkit-dev libqt4-opengl-dev
 fi
