e346aba02cb61d6d383173db1fcc810ce65a1bef
bill lam <bbill.lam@gmail.com>
2023-02-28 22:28:44 +0800

    github action

--- script/makewin.cmd
+++ script/makewin.cmd
@@ -3,18 +3,25 @@
 
 set PATH=C:\$GITHUB_WORKSPACE\qt\Qt\%1\win64_msvc2019_64\bin;%PATH%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 cd lib
 qmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 nmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 cd ..\main
 qmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 nmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 cd ..
 move bin\win-x86_64\release jqt-win
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 cd jqt-win
 powershell Compress-Archive * ..\jqt-win.zip
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 cd ..
 
 RD /S /Q release
@@ -25,15 +32,21 @@ RD /S /Q main\build
 set JQTSLIM=1
 cd lib
 qmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 nmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 cd ..\main
 qmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 nmake
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 cd ..
 move bin\win-x86_64\release jqt-win-slim
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 cd jqt-win-slim
 powershell Compress-Archive * ..\jqt-win-slim.zip
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 cd ..
 
 RD /S /Q release
