008f1a0f079462851754eb3f7c555813f2fbd5ab
bill lam <bbill.lam@gmail.com>
2023-05-12 13:36:19 +0800

    github windows

--- script/makewin.cmd
+++ script/makewin.cmd
@@ -1,9 +1,9 @@
 @REM Set up Windows SDK for 64bit
 @REM argument is Qt version, e.g. "5.15.2"
 
-set PATH=C:\$GITHUB_WORKSPACE\Qt\%1\win64_msvc2019_64\bin;%PATH%
+@rem set PATH=C:\$GITHUB_WORKSPACE\Qt\%1\win64_msvc2019_64\bin;%PATH%
+@rem CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 echo %PATH%
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 cd lib
