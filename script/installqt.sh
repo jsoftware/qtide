d443455651e20e33cded4ccfca1980d9cc7444dc
bill lam <bbill.lam@gmail.com>
2023-12-24 10:07:01 +0800

    yml

--- script/installqt.sh
+++ script/installqt.sh
@@ -6,6 +6,8 @@ g() { sudo pkg_add "$@"; }
 h() { sudo pkg install -y "$@"; }
 
 if [ "$1" = "linux" ] ; then
+sudo apt-get update -y
+f libssh-gcrypt-4
 f libpulse-dev
 f qtbase5-dev qtbase5-dev-tools
 f qtmultimedia5-dev libqt5multimediawidgets5
@@ -13,6 +15,7 @@ f libqt5opengl5 libqt5opengl5-dev
 f libqt5svg5 libqt5svg5-dev
 f qtwebengine5-dev libqt5websockets5-dev
 elif [ "$1" = "raspberry" ] ; then
+sudo apt-get update -y
 f libpulse-dev
 f qtbase5-dev qtbase5-dev-tools
 f qtmultimedia5-dev libqt5multimediawidgets5
@@ -20,6 +23,7 @@ f libqt5opengl5 libqt5opengl5-dev
 f libqt5svg5 libqt5svg5-dev
 f qtwebengine5-dev libqt5websockets5-dev
 elif [ "$1" = "raspberry-arm32" ] ; then
+sudo apt-get update -y
 f libqt4-dev libqt4-opengl-dev libqt4-svg
 elif [ "$1" = "openbsd" ] ; then
 g qtbase qtmultimedia qtsvg qttools qtwebengine qtwebsockets
