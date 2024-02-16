18db4f47da69b2cd60b3b14b314522a0e6ef2b92
bill lam <bbill.lam@gmail.com>
2024-02-16 17:34:54 +0800

    add files

---
+++ lib/base/pcombobox.cpp
@@ -0,0 +1,22 @@
+
+#include "pcombobox.h"
+
+// ---------------------------------------------------------------------
+PComboBox::PComboBox(QWidget *parent) : QComboBox(parent)
+{
+  acceptKeyBack=false;
+}
+
+// ---------------------------------------------------------------------
+void PComboBox::keyReleaseEvent(QKeyEvent *event)
+{
+#ifdef Q_OS_ANDROID
+  if ((event->key()==Qt::Key_Back) && (!isEditable()) && (!acceptKeyBack)) {
+    QComboBox::keyReleaseEvent(event);
+    event->ignore();
+    return;
+  } else QComboBox::keyReleaseEvent(event);
+#else
+  QComboBox::keyReleaseEvent(event);
+#endif
+}
