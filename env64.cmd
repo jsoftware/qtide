91851093ec0845c03633ae78e2f80ec738667380
bill lam <bbill.lam@gmail.com>
2025-06-02 10:33:58 +0800

    Qt 6.8.3

--- env64.cmd
+++ env64.cmd
@@ -1,5 +1,5 @@
 @REM Set up Windows SDK for 64bit
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat"
-set PATH=C:\Qt\Qt6.8.2\6.8.2\msvc2022_64\bin;%PATH%
+set PATH=C:\Qt\Qt6.8.3\6.8.3\msvc2022_64\bin;%PATH%
 set QMAKESPEC=win32-msvc
 @rem set JQTSLIM=JQTSLIM
