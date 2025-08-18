de67c7ca04a4b35fd28f438c36a2d39b12bfc6a1
bill lam <bbill.lam@gmail.com>
2025-08-18 21:43:29 +0800

    test

--- script/installqt-old.sh
+++ script/installqt-old.sh
@@ -5,9 +5,8 @@ f() { sudo apt-get install --no-install-recommends -y "$@"; }
 g() { sudo pkg_add "$@"; }
 h() { sudo pkg install -y "$@"; }
 
-if [ "$1" = "linux5" ] ; then
+if [ "$1" = "linux" ] ; then
 sudo apt-get update -y
-sudo apt-get upgrade -y
 f libssh-gcrypt-4
 f libpulse-dev
 f qtbase5-dev qtbase5-dev-tools
@@ -15,15 +14,23 @@ f qtmultimedia5-dev libqt5multimediawidgets5
 f libqt5opengl5 libqt5opengl5-dev
 f libqt5svg5 libqt5svg5-dev
 f qtwebengine5-dev libqt5websockets5-dev
-elif [ "$1" = "linux6" ] ; then
+elif [ "$1" = "raspberry" ] ; then
 sudo apt-get update -y
-sudo apt-get upgrade -y
 f libpulse-dev
-f qt6-base-dev qt6-base-dev-tools
-f qt6-multimedia-dev libqt6multimedia6 libqt6multimediawidgets6
-f libqt6opengl6t64 libqt6openglwidgets6t64
-f qt6-svg-dev libqt6svg6 libqt6svgwidgets6
-f qt6-webengine-dev qt6-webengine-dev-tools
+f qtbase5-dev qtbase5-dev-tools
+f qtmultimedia5-dev libqt5multimediawidgets5
+f libqt5opengl5 libqt5opengl5-dev
+f libqt5svg5 libqt5svg5-dev
+f qtwebengine5-dev libqt5websockets5-dev
+elif [ "$1" = "raspberry-arm32" ] ; then
+sudo apt-get update -y
+# f libqt4-dev libqt4-opengl-dev libqt4-svg
+f libpulse-dev
+f qtbase5-dev qtbase5-dev-tools
+f qtmultimedia5-dev libqt5multimediawidgets5
+f libqt5opengl5 libqt5opengl5-dev
+f libqt5svg5 libqt5svg5-dev
+f qtwebengine5-dev libqt5websockets5-dev
 elif [ "$1" = "openbsd" ] ; then
 g qtbase qtmultimedia qtsvg qttools qtwebengine qtwebsockets
 elif [ "$1" = "freebsd" ] ; then
