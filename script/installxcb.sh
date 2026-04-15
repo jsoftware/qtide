4eed163c921faebed08cceefe34f2c4191318025
bill lam <bbill.lam@gmail.com>
2026-04-15 14:49:08 +0800

    ip

---
+++ script/installxcb.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -evx
+
+# argument is linux|darwin|raspberry|android|openbsd|freebsd|wasm [x64|x86|arm64|armv6l]
+# wasm is experimental
+
+echo "pwd $(pwd)"
+echo "parameters $1 $2"
+echo "uname -m  $(uname -m)"
+echo "RUNNER_ARCH  $RUNNER_ARCH"
+
+if [ "$2" = "x64" ] ; then
+ arch=x86_64
+elif [ "$2" = "x86" ] ; then
+ arch=x86
+elif [ "$2" = "arm64" ] ; then
+ arch=aarch64
+elif [ "$2" = "armv6l" ] ; then
+ arch=armv6l
+elif [ "$1" = "wasm" ]; then
+ arch=wasm32
+else
+ arch="`uname -m`"
+fi
+
+f() {
+sudo apt-get install --no-install-recommends -y "$@" || true ;
+if [ $arch = "armv6l" ] ; then
+if [ "$RUNNER_ARCH" = "ARM64" ] ; then
+sudo apt-get install --no-install-recommends -y "$@":armhf || true ;
+fi
+fi
+}
+g() { sudo pkg_add "$@"; }
+h() { sudo pkg install -y "$@"; }
+
+if [ "$1" = "linux" ] || [ "$1" = "raspberry" ] ; then
+sudo apt-get update -y
+sudo apt-get upgrade -y
+f libxcb-cursor0
+fi
+elif [ "$1" = "openbsd" ] ; then
+g libxcb-cursor0
+elif [ "$1" = "freebsd" ] ; then
+g libxcb-cursor0
+fi
+
--- script/testinstall.sh
+++ script/testinstall.sh
@@ -1,22 +1,42 @@
 #!/bin/sh
 # set -evx
 
-# argument is linux|darwin|raspberry|android|openbsd|freebsd|wasm [x64|x86|arm64|armv6l] Qtver
+# argument is linux|darwin|raspberry|android|openbsd|freebsd|wasm [x64|x86|arm64|armv6l]
 # wasm is experimental
-# Qt version, e.g. "5.15.2"
 
 echo "pwd $(pwd)"
-echo "parameters $1 $2 $3"
+echo "parameters $1 $2"
 echo "uname -m  $(uname -m)"
 echo "RUNNER_ARCH  $RUNNER_ARCH"
+
+if [ "$2" = "x64" ] ; then
+ arch=x86_64
+elif [ "$2" = "x86" ] ; then
+ arch=x86
+elif [ "$2" = "arm64" ] ; then
+ arch=aarch64
+elif [ "$2" = "armv6l" ] ; then
+ arch=armv6l
+elif [ "$1" = "wasm" ]; then
+ arch=wasm32
+else
+ arch="`uname -m`"
+fi
 
 script/jinstall.sh -p qt1 -q slim --no-addons
 find qt1 
 script/jinstall.sh -p qt2 -q full --no-addons
 find qt2 
 if [ "$1" = "darwin" ] ; then
-  ./qt1/j9.7/jqt.app -js "exit 0[stderr JVERSION"
+  if [ "$arch" = "x86_64" ] ; then
+    ./qt1/j9.7/bin/jqtx.command -js "exit 0[stderr JVERSION"
+    ./qt2/j9.7/bin/jqtx.command -js "exit 0[stderr JVERSION"
+  else
+    ./qt1/j9.7/bin/jqt.command -js "exit 0[stderr JVERSION"
+    ./qt2/j9.7/bin/jqt.command -js "exit 0[stderr JVERSION"
+  fi
 else
   ./qt1/j9.7/jqt.sh -js "exit 0[stderr JVERSION"
+  ./qt2/j9.7/jqt.sh -js "exit 0[stderr JVERSION"
 fi
 
