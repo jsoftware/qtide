9c65887bbaceff3260b1bc9805bfd831e46e2b26
bill lam <bbill.lam@gmail.com>
2026-04-15 11:25:06 +0800

    ip

--- script/testinstall.sh
+++ script/testinstall.sh
@@ -15,8 +15,8 @@ find qt1
 script/jinstall.sh -p qt2 -q full --no-addons
 find qt2 
 if [ "$1" = "darwin" ] ; then
-  ./qt1/j9.7/jqt.app -js "exit 0[echo JVERSION"
+  ./qt1/j9.7/jqt.app -js "exit 0[stderr JVERSION"
 else
-  ./qt1/j9.7/jqt.sh -js "exit 0[echo JVERSION"
+  ./qt1/j9.7/jqt.sh -js "exit 0[stderr JVERSION"
 fi
 
